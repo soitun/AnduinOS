92e92a23d2a9c99a41082f25402b20af4798b3e8
Anduin Xue <anduin@aiursoft.com>
2025-05-13 17:02:05 +0000

    Add signal binding and cleanup functions for improved resource management

--- src/build.sh
+++ src/build.sh
@@ -10,6 +10,12 @@ export SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 source $SCRIPT_DIR/shared.sh
 source $SCRIPT_DIR/args.sh
 
+function bind_signal() {
+    print_ok "Bind signal..."
+    trap umount_on_exit EXIT
+    judge "Bind signal"
+}
+
 function check_host() {
 
     local os_ver
@@ -27,10 +33,10 @@ function check_host() {
 
 function clean() {
     print_ok "Cleaning up..."
+    sudo umount new_building_os/sys || sudo umount -lf new_building_os/sys || true
+    sudo umount new_building_os/proc || sudo umount -lf new_building_os/proc || true
     sudo umount new_building_os/dev || sudo umount -lf new_building_os/dev || true
     sudo umount new_building_os/run || sudo umount -lf new_building_os/run || true
-    sudo umount new_building_os/proc || sudo umount -lf new_building_os/proc || true
-    sudo umount new_building_os/sys || sudo umount -lf new_building_os/sys || true
     sudo rm -rf new_building_os || true
     judge "Clean up rootfs"
     sudo rm -rf image || true
@@ -335,8 +341,19 @@ EOF
     popd
 }
 
+function umount_on_exit() {
+    sleep 2
+    print_ok "Umount before exit..."
+    sudo umount new_building_os/sys || sudo umount -lf new_building_os/sys || true
+    sudo umount new_building_os/proc || sudo umount -lf new_building_os/proc || true
+    sudo umount new_building_os/dev || sudo umount -lf new_building_os/dev || true
+    sudo umount new_building_os/run || sudo umount -lf new_building_os/run || true
+    judge "Umount before exit"
+}
+
 # =============   main  ================
 cd $SCRIPT_DIR
+bind_signal
 check_host
 clean
 setup_host
