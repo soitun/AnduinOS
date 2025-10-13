821b998b1caa0e6e31ecd5b385260e514b5de393
Anduin Xue <anduin@aiursoft.com>
2025-10-13 15:37:23 +0000

    Update install.sh to use dynamic TARGET_NAME for ID in os-release

---
+++ src/mods/23-software-properties-common-patch/install.sh
@@ -0,0 +1,20 @@
+set -e                  # exit on error
+set -o pipefail         # exit on pipeline error
+set -u                  # treat unset variable as error
+
+print_ok "Patching /usr/bin/add-apt-repository to always identify as Ubuntu..."
+sudo sed -i.bak "/self.distro = get_distro()/a \        self.distro.id = 'ubuntu' # Patched" /usr/bin/add-apt-repository
+judge "Patch /usr/bin/add-apt-repository"
+#software-properties-common
+
+print_ok "Marking software-properties-common as held..."
+apt-mark hold software-properties-common
+judge "Mark software-properties-common as held"
+
+print_ok "Marking software-properties-common as not upgradeable..."
+cat << EOF > /etc/apt/preferences.d/no-upgrade-software-properties-common
+Package: software-properties-common
+Pin: release o=Ubuntu
+Pin-Priority: -1
+EOF
+judge "Create PIN file for software-properties-common"
